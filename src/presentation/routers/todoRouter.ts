6134bc46cab4f76c11a37c8ef67cde853ea8192f
Luis Martínez <lemartinezm@unitru.edu.pe>
2022-11-23 11:47:04 -0500

    test: todo router get endpoint

---
+++ src/presentation/routers/todoRouter.ts
@@ -0,0 +1,25 @@
+import { Request, Response, Router } from "express";
+import { CreateTodoUseCase } from "../../domain/interfaces/useCases/todos/createTodo";
+import { DeleteTodoUseCase } from "../../domain/interfaces/useCases/todos/deleteTodo";
+import { GetAllTodosUseCase } from "../../domain/interfaces/useCases/todos/getAllTodos";
+import { UpdateTodoUseCase } from "../../domain/interfaces/useCases/todos/updateTodo";
+
+export function TodoRouter(
+  getAllTodosUseCase: GetAllTodosUseCase,
+  createTodoUseCase: CreateTodoUseCase,
+  deleteTodoUseCase: DeleteTodoUseCase,
+  updateTodoUseCase: UpdateTodoUseCase
+) {
+  const router = Router();
+
+  router.get("/", async (req: Request, res: Response) => {
+    try {
+      const todos = await getAllTodosUseCase.execute();
+      res.status(200).send(todos);
+    } catch (err) {
+      res.status(500).send({ message: "Error fetching todos" });
+    }
+  });
+
+  return router;
+}
---
+++ tests/presentation/routers/todoRouter.test.ts
@@ -0,0 +1,110 @@
+import request from "supertest";
+import server from "../../../src/server";
+import { Todo } from "../../../src/domain/entities/todo";
+import { TodoRouter } from "../../../src/presentation/routers/todoRouter";
+import { GetAllTodosUseCase } from "../../../src/domain/interfaces/useCases/todos/getAllTodos";
+import { CreateTodoUseCase } from "../../../src/domain/interfaces/useCases/todos/createTodo";
+import { DeleteTodoUseCase } from "../../../src/domain/interfaces/useCases/todos/deleteTodo";
+import { UpdateTodoUseCase } from "../../../src/domain/interfaces/useCases/todos/updateTodo";
+
+describe("Todo Router", () => {
+  class MockGetAllTodosUseCase implements GetAllTodosUseCase {
+    execute(): Promise<Todo[]> {
+      throw new Error("Method not implemented.");
+    }
+  }
+
+  class MockCreateTodoUseCase implements CreateTodoUseCase {
+    execute(InputData: Todo): Promise<boolean> {
+      throw new Error("Method not implemented.");
+    }
+  }
+
+  class MockDeleteTodoUseCase implements DeleteTodoUseCase {
+    execute(id: string): Promise<boolean> {
+      throw new Error("Method not implemented.");
+    }
+  }
+
+  class MockUpdateTodoUseCase implements UpdateTodoUseCase {
+    execute(id: string, dataToUpdate: Partial<Todo>): Promise<boolean> {
+      throw new Error("Method not implemented.");
+    }
+  }
+
+  let mockGetAllTodosUseCase: MockGetAllTodosUseCase;
+  let mockCreateTodoUseCase: MockCreateTodoUseCase;
+  let mockDeleteTodoUseCase: MockDeleteTodoUseCase;
+  let mockUpdateTodoUseCase: MockUpdateTodoUseCase;
+
+  beforeAll(() => {
+    mockGetAllTodosUseCase = new MockGetAllTodosUseCase();
+    mockCreateTodoUseCase = new MockCreateTodoUseCase();
+    mockDeleteTodoUseCase = new MockDeleteTodoUseCase();
+    mockUpdateTodoUseCase = new MockUpdateTodoUseCase();
+
+    server.use(
+      "/todo",
+      TodoRouter(
+        mockGetAllTodosUseCase,
+        mockCreateTodoUseCase,
+        mockDeleteTodoUseCase,
+        mockUpdateTodoUseCase
+      )
+    );
+  });
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("Todo Router should be defined", () => {
+    expect(TodoRouter).toBeDefined();
+  });
+
+  describe("GET /todo", () => {
+    it("should return all todos", async () => {
+      const ExpectedResult: Todo[] = [
+        {
+          id: "1",
+          name: "My todo",
+          description: "My description",
+          createdAt: new Date(),
+          deadline: new Date(),
+          priority: "low",
+          completed: false,
+          creator: "1",
+        },
+      ];
+
+      jest
+        .spyOn(mockGetAllTodosUseCase, "execute")
+        .mockImplementation(() => Promise.resolve(ExpectedResult));
+
+      const response = await request(server).get("/todo");
+
+      expect(mockGetAllTodosUseCase.execute).toHaveBeenCalled();
+      expect(response.status).toBe(200);
+      expect(response.body).toStrictEqual([
+        {
+          ...ExpectedResult[0],
+          createdAt: ExpectedResult[0].createdAt.toJSON(),
+          deadline: ExpectedResult[0].deadline.toJSON(),
+        },
+      ]);
+    });
+
+    it("should return error with status 500", async () => {
+      const message = "Error fetching todos";
+
+      jest
+        .spyOn(mockGetAllTodosUseCase, "execute")
+        .mockImplementation(() => Promise.reject(Error()));
+
+      const response = await request(server).get("/todo");
+
+      expect(response.status).toBe(500);
+      expect(response.body).toStrictEqual({ message });
+    });
+  });
+});
