eeef7838f4d99054560471b4433b4328b6a203d1
Luis Martínez <lemartinezm@unitru.edu.pe>
2022-11-28 18:35:31 -0500

    feat: add user endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,27 @@
 import mongoose from "mongoose";
 import server from "./server";
+
 import { TodoRouter } from "./presentation/routers/todoRouter";
 import { UserRouter } from "./presentation/routers/userRouter";
-import { GetAllTodos } from "./domain/useCases/todos/getAllTodos";
+
 import { TodoControllerImpl } from "./domain/controllers/todoController";
+import { UserControllerImpl } from "./domain/controllers/userController";
 import { MongoDBTodoDataSource } from "./data/dataSource/mongodb/mongodbTodoDataSource";
+import { MongoDBUserDataSource } from "./data/dataSource/mongodb/mongodbUserDataSource";
+
 import { Database } from "./data/interfaces/dataSources/database";
+
 import { Todo } from "./data/dataSource/mongodb/models/todoModel";
+import { GetAllTodos } from "./domain/useCases/todos/getAllTodos";
 import { CreateTodo } from "./domain/useCases/todos/createTodo";
 import { DeleteTodo } from "./domain/useCases/todos/deleteTodo";
 import { UpdateTodo } from "./domain/useCases/todos/updateTodo";
+
+import { User } from "./data/dataSource/mongodb/models/userModel";
+import { GetAllUsers } from "./domain/useCases/users/getAllUsers";
+import { CreateUser } from "./domain/useCases/users/createUser";
+import { DeleteUser } from "./domain/useCases/users/deleteUser";
+import { UpdateUser } from "./domain/useCases/users/updateUser";
 
 (async () => {
   await mongoose.connect("mongodb://localhost:27017/todoapp");
@@ -21,6 +33,15 @@ import { UpdateTodo } from "./domain/useCases/todos/updateTodo";
     deleteOne: async (query) => await Todo.deleteOne(query),
     updateOne: async (query, dataToUpdate) =>
       await Todo.updateOne(query, dataToUpdate),
+  };
+
+  const userDatabase: Database = {
+    find: async (query) => await User.find(query),
+    findOne: async (query) => await User.findOne(query),
+    insertOne: async (query) => await User.create(query),
+    deleteOne: async (query) => await User.deleteOne(query),
+    updateOne: async (query, dataToUpdate) =>
+      await User.updateOne(query, dataToUpdate),
   };
 
   const todoMiddleware = TodoRouter(
@@ -38,7 +59,23 @@ import { UpdateTodo } from "./domain/useCases/todos/updateTodo";
     )
   );
 
+  const userMiddleware = UserRouter(
+    new GetAllUsers(
+      new UserControllerImpl(new MongoDBUserDataSource(userDatabase))
+    ),
+    new CreateUser(
+      new UserControllerImpl(new MongoDBUserDataSource(userDatabase))
+    ),
+    new DeleteUser(
+      new UserControllerImpl(new MongoDBUserDataSource(userDatabase))
+    ),
+    new UpdateUser(
+      new UserControllerImpl(new MongoDBUserDataSource(userDatabase))
+    )
+  );
+
   server.use("/todo", todoMiddleware);
+  server.use("/user", userMiddleware);
   server.listen(4000, () => {
     console.log("Running server on port 4000");
   });
